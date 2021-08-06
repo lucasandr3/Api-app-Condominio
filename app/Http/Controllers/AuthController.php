044a114100eb51347b07c0bf932f71dc3610f173
lucasandr3 <lucasvieiraandrade58@gmail.com>
2021-08-06 00:49:55 -0300

    Implementando registro do usuario

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    //
+    public function unauthorized()
+    {
+        return response()->json(['error' => 'Não Autorizado'], 401);
+    }
+
+    public function register(Request $request)
+    {
+        $array = ['error' => ''];
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'cpf' => 'required|digits:11|unique:users,cpf',
+            'password' => 'required',
+            'password_confirm' => 'required|same:password'
+        ]);
+
+        if(!$validator->fails()) {
+
+
+
+        } else {
+            return $array['error'] = $validator->errors()->first();
+        }
+
+        return $array;
+    }
 }
